aae50340e83a4e94dcebfb0baa8231979359e6b3
imthegngrbrdmn <maarnold7@alaska.edu>
2022-02-24 14:52:34 -0900

    hw3 DONE

--- lexit.lua
+++ lexit.lua
@@ -42,17 +42,19 @@ local function isWhitespace(c)
 end
 
 local function isPrintableASCII(c)
-  if c:len() == 1 and (c >= " " or c <= "~") then
+  if c:len() == 1 and c >= " " and c <= "~" then
     return true
+  else
+    return false
   end
-  return false
 end
 
 local function isIllegal(c)
-  if c:len() ~= 1 or isWhitespace(c) or isPrintableASCII(c) then
+  if isWhitespace(c) or isPrintableASCII(c) then
     return false
+  else
+    return true
   end
-  return true
 end
 
 -- Lexer
@@ -86,6 +88,7 @@ function lexit.lex(program)
   local MODULO = 14
   local RBRACKET = 15
   local LBRACKET = 16
+  local COMMENT = 17
   
   local function currChar()
     return program:sub(pos,pos)
@@ -95,16 +98,38 @@ function lexit.lex(program)
     return program:sub(pos+1,pos+1)
   end
   
-  local function add1()
-    lexstr = lexstr .. currChar()
+  local function next2Char()
+    return program:sub(pos+2,pos+2)
   end
   
   local function drop1()
     pos = pos + 1
   end
   
+  local function add1()
+    lexstr = lexstr .. currChar()
+    drop1()
+  end
   
   local function skipToNextLexeme()
+    while true do
+      while isWhitespace(currChar()) do
+        drop1()
+      end
+      if currChar() ~= "#" then
+        break
+      end
+      drop1()
+      while true do
+        if currChar() == "\n" then
+          drop1()
+          break
+        elseif currChar() == "" then
+          return
+        end
+        drop1()
+      end
+    end
   end
   
   --State handlers
@@ -113,22 +138,211 @@ function lexit.lex(program)
   end
   
   local function handle_START()
-    add1()
-    state = DONE
-    category = lexit.PUNCT
+    if isIllegal(ch) then
+      add1()
+      state = DONE
+      category = lexit.MAL
+    elseif isLetter(ch) or ch == "_" then
+      add1()
+      state = LETTER
+    elseif isDigit(ch) then
+      add1()
+      state = DIGIT
+    elseif ch == '"' or ch == "'" then
+      add1()
+      state = QUOTE
+    elseif ch == '=' then
+      add1()
+      state = EQUAL
+    elseif ch == '!' then
+      add1()
+      state = BANG
+    elseif ch == '<' then
+      add1()
+      state = LESSTHAN
+    elseif ch == '>' then
+      add1()
+      state = GREATERTHAN
+    elseif ch == '+' then
+      add1()
+      state = PLUS
+    elseif ch == '-' then
+      add1()
+      state = MINUS
+    elseif ch == '*' then
+      add1()
+      state = STAR
+    elseif ch == '/' then
+      add1()
+      state = FSLASH
+    elseif ch == '%' then
+      add1()
+      state = MODULO
+    elseif ch == '[' then
+      add1()
+      state = LBRACKET
+    elseif ch == ']' then
+      add1()
+      state = RBRACKET
+    elseif ch == '#' then
+      add1()
+      state = COMMENT
+    elseif isWhitespace(ch) then
+      drop1()
+    else
+      add1()
+      state = DONE
+      category = lexit.PUNCT
+    end
   end
   
   local function handle_LETTER()
+    if isLetter(ch) or ch == '_' or isDigit(ch) then
+      add1()
+    else
+      state = DONE
+      if lexstr == "and" or lexstr == "char" or lexstr == "cr" or lexstr == "elif" or 
+      lexstr == "else" or lexstr == "false" or lexstr == "func" or lexstr == "if" or 
+      lexstr == "not" or lexstr == "or" or lexstr == "print" or lexstr == "read" or 
+      lexstr == "return" or lexstr == "true" or lexstr == "while" then
+        category = lexit.KEY
+      else
+        category = lexit.ID
+      end
+    end
   end
   
   local function handle_DIGIT()
+    if isDigit(ch) then
+      add1()
+    elseif (ch == "e" or ch == "E") and (isDigit(nextChar()) or (nextChar() == '+' and isDigit(next2Char()))) then
+      add1()
+      state = EXPONENT
+    else
+      state = DONE
+      category = lexit.NUMLIT
+    end
   end
   
+  local function handle_EXPONENT()
+    if isDigit(ch) or ch == "+" then
+      add1()
+    else
+      state = DONE
+      category = lexit.NUMLIT
+    end
+  end
   
+  local function handle_QUOTE()
+    local qc = lexstr:sub(1,1)
+    while currChar() ~= qc do
+      if currChar() == '\n' or currChar() == '' then
+        category = lexit.MAL
+        state = DONE
+        return
+      end
+      add1()
+    end
+    add1()
+    state = DONE
+    category = lexit.STRLIT
+  end
+  
+  local function handle_EQUAL()
+    if currChar() == '=' then
+      add1()
+    end
+    state = DONE
+    category = lexit.OP
+  end
+  
+  local function handle_BANG()
+    if currChar() == '=' then
+      add1()
+      state = DONE
+      category = lexit.OP
+    else
+      state = DONE
+      category = lexit.PUNCT
+    end
+  end
+  
+  local function handle_LESSTHAN()
+    if currChar() == '=' then
+      add1()
+    end
+    state = DONE
+    category = lexit.OP
+  end
+  
+  local function handle_GREATERTHAN()
+    if currChar() == '=' then
+      add1()
+    end
+    state = DONE
+    category = lexit.OP
+  end
+  
+  local function handle_PLUS()
+    state = DONE
+    category = lexit.OP
+  end
+  
+  local function handle_MINUS()
+    state = DONE
+    category = lexit.OP
+  end
+  
+  local function handle_STAR()
+    state = DONE
+    category = lexit.OP
+  end
+  
+  local function handle_FSLASH()
+    state = DONE
+    category = lexit.OP
+  end
+  
+  local function handle_MODULO()
+    state = DONE
+    category = lexit.OP
+  end
+  
+  local function handle_RBRACKET()
+    state = DONE
+    category = lexit.OP
+  end
+  
+  local function handle_LBRACKET()
+    state = DONE
+    category = lexit.OP
+  end
+  
+
+  local function handle_COMMENT()
+    skipToNextLexeme()
+  end
+
   
   handlers = {
     [DONE] = handle_DONE,
     [START] = handle_START,
+    [LETTER] = handle_LETTER,
+    [DIGIT] = handle_DIGIT,
+    [EXPONENT] = handle_EXPONENT,
+    [QUOTE] = handle_QUOTE,
+    [EQUAL] = handle_EQUAL,
+    [BANG] = handle_BANG,
+    [LESSTHAN] = handle_LESSTHAN,
+    [GREATERTHAN] = handle_GREATERTHAN,
+    [PLUS] = handle_PLUS,
+    [MINUS] = handle_MINUS,
+    [STAR] = handle_STAR,
+    [FSLASH] = handle_FSLASH,
+    [MODULO] = handle_MODULO,
+    [RBRACKET] = handle_RBRACKET,
+    [LBRACKET] = handle_LBRACKET,
+    [COMMENT] = handle_COMMENT,
     }
   
   local function getLexeme(dummy1, dummy2)
